19ac756212cd9ee84b7ff36f0aff98b55f912bec
dujinqiao <duzi1996@qq.com>
2017-05-07 20:06:13 +0800

    add manhattan.py

---
+++ test.py
@@ -0,0 +1,12 @@
+a = ["app", "leb", "element"]
+for i in a:
+    print i
+
+for i in range(3):
+    print i
+
+for letter in 'Python':
+    if letter == 'h':
+        pass
+        print 'This is pass block'
+    print 'Current : ', letter
