4dcc40fe30fb7b51a75feac6ac8d815062ae4c64
dujinqiao <duzi1996@qq.com>
2017-05-07 20:38:28 +0800

    add computeNearestNeighbor

---
+++ recommend-relative/computeNearestNeighbor.py
@@ -0,0 +1,15 @@
+import manhattan
+import math
+
+
+def computeNearestNeighbor(username, users):
+    distances = []
+    for user in users:
+        if user != username:
+            distance = manhattan.manhattan(users[user], users[username])
+            distances.append((distance, user))
+    distances.sort()
+    return distances
+
+
+print computeNearestNeighbor("Angelica", manhattan.users)
